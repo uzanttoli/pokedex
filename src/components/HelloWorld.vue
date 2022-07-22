b695b8a994ad1d6baf3ac06d62e412432ff2018f
unknown <uosantos@outlook.com.br>
2022-07-22 01:44:45 -0300

    criacao de projeto pokedex com POKEAPI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="main">
+    <v-row>
+      <v-col v-for="(item, i) in pokemons" :key="i">
+        <router-link to="/" style="text-decoration: none">
+          <ListPokemon :name="item.name" :url="item.url" />
+        </router-link>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import ListPokemon from "./cards/ListPokemon.vue";
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    ListPokemon,
+  },
+  data: () => ({
+    pokemons: [],
+  }),
+  created() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#main {
+  height: 100vh;
+  background-image: linear-gradient(
+    to bottom,
+    rgb(48, 160, 204),
+    rgb(255, 255, 255)
+  );
+  position: absolute;
+  width: 100%;
 }
 </style>
---
+++ src/components/cards/ListPokemon.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto rounded-t-xl rounded-b-xl"
+    style="position: relative; margin-top: 10px"
+    max-width="300"
+    width="250"
+  >
+    <div id="img-pokemon">
+      <v-img class="pa-2" width="100%" :src="pokemon.front"></v-img>
+    </div>
+
+    <v-card elevation="0" class="rounded-t-xl mt-n12" height="200">
+      <v-card-title>{{ name }}</v-card-title>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-col class="text-center" v-for="(item, i) in type" :key="i">
+            <v-chip class="ma-2">
+              {{ item.type.name }}
+            </v-chip>
+          </v-col>
+
+          <!-- <div class="grey--text ms-4">4.5 (413)</div> -->
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    url: {
+      type: String,
+      require: true,
+    },
+  },
+  data: () => ({
+    loading: false,
+    selection: 1,
+    pokemon: {
+      front: "",
+    },
+    type: [],
+  }),
+  methods: {
+    pokemonId() {
+      axios.get(this.url).then((res) => {
+        this.type = res.data.types;
+        this.pokemon.front =
+          res.data.sprites.other["official-artwork"].front_default;
+      });
+    },
+    reserve() {
+      this.loading = true;
+
+      setTimeout(() => (this.loading = false), 2000);
+    },
+  },
+  created() {
+    this.pokemonId();
+  },
+};
+</script>
+<style scoped>
+#img-pokemon {
+  width: 100px;
+  height: 155px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
